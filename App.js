ac921e0a67798a2cfbcc127fc401f7a596dbd9a5
HTM1000 <ID1355342@outlook.com>
2022-08-09 15:59:55 -0300

    deliveroo-clone

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TailwindProvider } from "tailwindcss-react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider } from 'react-redux';
+import { store } from "./store"
+
+import HomeScreen from "./src/screens/Home/index.js";
+import RestaurantScreen from "./src/screens/Restaurant/index.js";
+import BasketScreen from "./src/screens/Basket/index.js";
+import PreparingOrderScreen from "./src/screens/PreparingOrder/index.js";
+import DeliveryScreen from "./src/screens/Delivery/index.js";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Provider store={store}>
+        <TailwindProvider>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+            <Stack.Screen 
+              name="Basket" 
+              component={BasketScreen} 
+              options={{ presentation: "modal", headerShown: false }}
+            />
+            <Stack.Screen 
+              name="PreparingOrder" 
+              component={PreparingOrderScreen} 
+              options={{ presentation: "fullScreenModal", headerShown: false }}
+             />
+            <Stack.Screen 
+              name="Delivery"
+              component={DeliveryScreen}
+              options={{ presentation: "fullScreenModal", headerShown: false }}
+            />
+          </Stack.Navigator>
+        </TailwindProvider>
+      </Provider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
